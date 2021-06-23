558955e39e55f79eff1a4f3d2d4c8c91242229fd
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-06-23 15:49:09 -0400

    added inquirer and typing out questions.

--- index.js
+++ index.js
@@ -1,31 +1,85 @@
+const inquirer = require('inquirer');
+
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-const readMeInput = {
-    title: "",
-    description: "",
-    tableOfContents: "",
-    installation: "",
-    usage: "",
-    license: "",
-    contributing: "",
-    tests: "",
-    questions: "",
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions 
+
+
+
+    inquirer
+    .prompt([{// WHEN I enter my project title
+            type: 'input',
+            message: "What is the title of your project's title?",
+            name: 'title',
+        },
+        {// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+            type: 'input',
+            message: 'Please type a description of the project?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Please type any installation instructions?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Please type any usage information to list?',
+            name: 'usage',
+        }, 
+        {
+            type: 'input',
+            message: 'Please type any contribution guidelines?',
+            name: 'guidelines',
+        }, 
+        {
+            type: 'input',
+            message: 'Please type any test instructions',
+            name: 'testInstr',
+        }, 
+        {// WHEN I choose a license for my application from a list of options
+            type: 'list',
+            message: 'Are you listing a license?',
+            name: 'license',
+        }, 
+        {
+            type: 'input',
+            message: 'What is the name of your github account',
+            name: 'gitAnswer',
+          },
+        {
+            type: 'input',
+            message: 'What is your email address',
+            name: 'emailAnswer',
+          },
+          
+    ])
+    .then((response) =>
+         
+       
+    );
+
+
+
+
 
 }
 
 // WHEN I enter my project title
 // THEN this is displayed as the title of the README
-var projTitle 
 
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
 // WHEN I enter my GitHub username
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
 // WHEN I enter my email address
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
 // ----------------------------------------------------------------------------------------------------------------------
@@ -43,6 +97,3 @@ function init() {}
 
 // Function call to initialize app
 init();
-
-
-
